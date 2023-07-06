8d9a28a72902854fe32b626247195a4029478ced
Ak2529 <arunkarthik2529@gmail.com>
2023-07-06 11:44:57 +0530

    Website using apache

---
+++ website.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+PACKAGE="httpd wget unzip"
+URL="https://www.free-css.com/assets/files/free-css-templates/download/page292/simply-amazed.zip"
+
+yum install $PACKAGE -y
+
+mkdir /tmp/website
+cd /tmp/website
+
+unzip simply-amazed.zip
+
+sudo cp -r 2123_simply_amazed/* /var/www/html/
+
+systemctl restart httpd
+systemctl enable httpd
+
+ifconfig
+
+
